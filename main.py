81671694cb9e876d0da049b6450a0b1e161e117a
Amogh Arya <amogharya2@gmail.com>
2023-03-27 22:56:29 +0530

    UPDATE: added files for deokoymnet

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import pyrebase
 
-app = FastAPI()
+firebaseConfig = {
+  "apiKey": "AIzaSyC_L7bacXhCfshzd35fJdyH_2prVU7FoeM",
+  "authDomain": "ai-sight-471fa.firebaseapp.com",
+  "projectId": "ai-sight-471fa",
+  "storageBucket": "ai-sight-471fa.appspot.com",
+  "messagingSenderId": "90293971390",
+  "appId": "1:90293971390:web:04b27715f6059d448831ef",
+  "measurementId": "G-14P2V3YKET",
+  "serviceAccount": "serviceAccount.json",
+  "databaseURL":"https://ai-sight-471fa-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+}
+
+app = FastAPI(max_request_size=10000000)
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage= firebase.storage()
+
+origins = ["*"]
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=["*"],
+
+)
+
+@app.get("/")
+def root():
+    return{
+        "message": "hello world"
+    }
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
+    storage.child("picture").put(file)
     return {"filename": file.filename}
