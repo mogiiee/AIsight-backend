87006cd935df13a1f931badc6e0eae0442c042ae
Amogh Arya <amogharya2@gmail.com>
2023-03-27 20:18:33 +0530

    UPDATE: main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
