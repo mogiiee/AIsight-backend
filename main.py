9813c7e00bf0d01a873c03e3be9ff04dde0b4a23
Amogh Arya <amogharya2@gmail.com>
2023-03-27 23:10:52 +0530

    UPDATE: render deployment

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import pyrebase
 
-firebaseConfig = {
-  "apiKey": "AIzaSyC_L7bacXhCfshzd35fJdyH_2prVU7FoeM",
-  "authDomain": "ai-sight-471fa.firebaseapp.com",
-  "projectId": "ai-sight-471fa",
-  "storageBucket": "ai-sight-471fa.appspot.com",
-  "messagingSenderId": "90293971390",
-  "appId": "1:90293971390:web:04b27715f6059d448831ef",
-  "measurementId": "G-14P2V3YKET",
-  "serviceAccount": "serviceAccount.json",
-  "databaseURL":"https://ai-sight-471fa-default-rtdb.asia-southeast1.firebasedatabase.app/"
-
-}
+# firebaseConfig = {
+#   "apiKey": "AIzaSyC_L7bacXhCfshzd35fJdyH_2prVU7FoeM",
+#   "authDomain": "ai-sight-471fa.firebaseapp.com",
+#   "projectId": "ai-sight-471fa",
+#   "storageBucket": "ai-sight-471fa.appspot.com",
+#   "messagingSenderId": "90293971390",
+#   "appId": "1:90293971390:web:04b27715f6059d448831ef",
+#   "measurementId": "G-14P2V3YKET",
+#   "serviceAccount": "serviceAccount.json",
+#   "databaseURL":"https://ai-sight-471fa-default-rtdb.asia-southeast1.firebasedatabase.app/"
+# }
 
 app = FastAPI(max_request_size=10000000)
-firebase = pyrebase.initialize_app(firebaseConfig)
-storage= firebase.storage()
+# firebase = pyrebase.initialize_app(firebaseConfig)
+# storage= firebase.storage()
 
 origins = ["*"]
 
@@ -41,5 +40,5 @@ def root():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    storage.child("picture").put(file)
+    # storage.child("picture").put(file)
     return {"filename": file.filename}
