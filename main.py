7486b1544d6e61399d0f7797cfd398d9688a238a
Amogh Arya <amogharya2@gmail.com>
2023-03-28 01:38:59 +0530

    firebase support

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import pyrebase
+import io
+import firebase_admin
+from firebase_admin import credentials, storage
 
-# firebaseConfig = {
-#   "apiKey": "AIzaSyC_L7bacXhCfshzd35fJdyH_2prVU7FoeM",
-#   "authDomain": "ai-sight-471fa.firebaseapp.com",
-#   "projectId": "ai-sight-471fa",
-#   "storageBucket": "ai-sight-471fa.appspot.com",
-#   "messagingSenderId": "90293971390",
-#   "appId": "1:90293971390:web:04b27715f6059d448831ef",
-#   "measurementId": "G-14P2V3YKET",
-#   "serviceAccount": "serviceAccount.json",
-#   "databaseURL":"https://ai-sight-471fa-default-rtdb.asia-southeast1.firebasedatabase.app/"
-# }
+mainAccount=  "serviceAccount.json"
 
-app = FastAPI(max_request_size=10000000)
-# firebase = pyrebase.initialize_app(firebaseConfig)
-# storage= firebase.storage()
+
+firebaseConfig = {
+  "apiKey": "AIzaSyC_L7bacXhCfshzd35fJdyH_2prVU7FoeM",
+  "authDomain": "ai-sight-471fa.firebaseapp.com",
+  "projectId": "ai-sight-471fa",
+  "storageBucket": "ai-sight-471fa.appspot.com",
+  "messagingSenderId": "90293971390",
+  "appId": "1:90293971390:web:04b27715f6059d448831ef",
+  "measurementId": "G-14P2V3YKET",
+  "serviceAccount": mainAccount,
+  "databaseURL":"gs://ai-sight-471fa.appspot.com"
+}
+
+app = FastAPI()
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage= firebase.storage()
+cred = credentials.Certificate("serviceAccount.json")
+
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'gs://ai-sight-471fa.appspot.com'
+})
 
 origins = ["*"]
 
@@ -38,7 +50,23 @@ def root():
     }
 
 
+# @app.post("/uploadfile/")
+# async def create_upload_file(file: UploadFile = File(...)):
+#     storage.child("picture").put(file)
+#     return {"filename": file.filename}
+
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    # storage.child("picture").put(file)
-    return {"filename": file.filename}
+    file_content = await file.read()  # Read the file's content
+    file_size = len(file_content)  # Determine the file's size
+    bucket = storage.bucket
+    blob = bucket.blob(file.filename)
+    blob.upload_from_string(file_content, content_type='image/jpeg')
+    blob.make_public()
+    
+    # Get the download URL of the uploaded file
+    url = blob.public_url
+    
+    # Return the download URL to the client
+    return {'url': url}
