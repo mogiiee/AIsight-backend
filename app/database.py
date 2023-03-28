f7c04f6b61c6e7fc5a0bbe9775fab72712f30ce8
Amogh Arya <amogharya2@gmail.com>
2023-03-28 16:07:10 +0530

    refactor pt 3

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from . import exporter
 import pymongo
 
 
-cluster = pymongo.MongoClient(exporter.real_cluster)
+cluster = pymongo.MongoClient()
+
+my_cluster = cluster(exporter.realcluster)
 
 db = cluster[exporter.db_name]
 
-user_collection = db[exporter.collection]
+user_collection = db[exporter.full_collection]
 
--- app/exporter.py
+++ app/exporter.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 firebase_config = os.environ.get("firebaseConfig")
 
-real_cluster = os.environ.get("CLUSTER")
+realcluster = os.environ.get("CLUSTER")
 db_name = os.environ.get("DB")
-collection = os.environ.get("USER_COLLECTION")
+full_collection = os.environ.get("USER_COLLECTION")
