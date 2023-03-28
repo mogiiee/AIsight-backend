d37700d4b3e7e502e12b0547e5d4e1483773630f
Amogh Arya <amogharya2@gmail.com>
2023-03-28 16:25:19 +0530

    refactor pt 5

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ import pymongo
 
 cluster = pymongo.MongoClient("mongodb://amogh:amogh@ac-ztxn30c-shard-00-00.esico7b.mongodb.net:27017,ac-ztxn30c-shard-00-01.esico7b.mongodb.net:27017,ac-ztxn30c-shard-00-02.esico7b.mongodb.net:27017/?ssl=true&replicaSet=atlas-i1fgx7-shard-0&authSource=admin&retryWrites=true&w=majority")
 
-db = cluster[exporter.db_name]
+db = cluster["make-a-thon"]
 
-user_collection = db[exporter.full_collection]
+user_collection = db["users"]
 
