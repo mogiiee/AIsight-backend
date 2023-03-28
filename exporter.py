ef3b7629ee1ceb8997d7f49dfea8c044a56fcd4d
Amogh Arya <amogharya2@gmail.com>
2023-03-28 14:17:27 +0530

    ADD:experter

--- exporter.py
+++ exporter.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 firebase_config = os.environ.get("firebaseConfig")
+
+cluster = os.environ.get("CLUSTER")
+database = os.environ.get("DB")
+collection = os.environ.get("USER_COLLECTION")
