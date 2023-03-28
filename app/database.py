440a499048e0c63ea13064a356d24cac0deecf92
Amogh Arya <amogharya2@gmail.com>
2023-03-28 16:19:33 +0530

    refactor pt 4

--- app/database.py
+++ app/database.py
@@ -2,9 +2,7 @@ from . import exporter
 import pymongo
 
 
-cluster = pymongo.MongoClient()
-
-my_cluster = cluster(exporter.realcluster)
+cluster = pymongo.MongoClient("mongodb://amogh:amogh@ac-ztxn30c-shard-00-00.esico7b.mongodb.net:27017,ac-ztxn30c-shard-00-01.esico7b.mongodb.net:27017,ac-ztxn30c-shard-00-02.esico7b.mongodb.net:27017/?ssl=true&replicaSet=atlas-i1fgx7-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 db = cluster[exporter.db_name]
 
